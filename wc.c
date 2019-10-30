5487026b65329441a8c7cd470e0980705c5c8bfe
Daniel Konečný <dkonecny97@gmail.com>
2019-10-30 11:18:03 +0100

    Add number of params condition

--- wc.c
+++ wc.c
@@ -2,11 +2,15 @@
 #include <string.h>
 
 int main(int argc, char* argv[]) {
-	argc++;
+	if(argc != 3) {
+		fprintf(stderr, "ERROR: Number of parameters should be 2, not %d!\n", argc-1);
+		return -1;
+	}
+
 	FILE *file = fopen(argv[2], "r");
 	if(file == NULL) {
 		fprintf(stderr, "ERROR: File %s not found!\n", argv[2]);
-		return -1;
+		return -2;
 	}
 
 	if((strcmp("-c", argv[1])) == 0) {
